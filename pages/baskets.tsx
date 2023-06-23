a7ea7e60351d600a4b70d99f4bbe29b7811df47f
milan <milanjulinac996@gmail.com>
2023-06-23 08:51:35 +0200

    gb-56 added filters for baskets type

--- pages/baskets.tsx
+++ pages/baskets.tsx
@@ -24,6 +24,11 @@ export default function Baskets() {
 	const [page, setPage] = useState(1);
 	const [limit, setLimit] = useState(10);
 	const [totalPages, setTotalPages] = useState(0);
+	const [nameFilter, setNameFilter] = useState("");
+	const [colorFilter, setColorFilter] = useState("");
+	const [fromPriceFilter, setFromPriceFilter] = useState("");
+	const [toPriceFilter, setToPriceFilter] = useState("");
+	const [shouldFetchData, setShouldFetchData] = useState(false);
 
 	const fetchData = async () => {
 		try {
@@ -33,16 +38,27 @@ export default function Baskets() {
 					params: {
 						page,
 						limit,
+						name: nameFilter,
+						color: colorFilter,
+						priceFrom: fromPriceFilter,
+						priceTo: toPriceFilter,
 					},
 				}
 			);
 			setItems(response.data.basketTypes.basketTypes);
 			setTotalPages(Math.ceil(response.data.basketTypes.total / 10));
 			setIsLoading(false);
+			setShouldFetchData(false);
 		} catch (error) {
 			console.log(error);
 		}
 	};
+
+	useEffect(() => {
+		if (shouldFetchData) {
+			fetchData();
+		}
+	}, [shouldFetchData]);
 
 	useEffect(() => {
 		fetchData();
@@ -83,6 +99,20 @@ export default function Baskets() {
 		return paginationLinks;
 	};
 
+	const applyFilers = () => {
+		setShouldFetchData(true);
+		setPage(1);
+	};
+
+	const clearFilters = () => {
+		setNameFilter("");
+		setColorFilter("");
+		setFromPriceFilter("");
+		setToPriceFilter("");
+		setShouldFetchData(true);
+		setPage(1);
+	};
+
 	return (
 		<Layout>
 			<h1>Basket Page</h1>
@@ -91,6 +121,94 @@ export default function Baskets() {
 				<p>Loading...</p>
 			) : (
 				<>
+					<div className="filters text-white p-4 mt-2 mb-2 rounded border border-blue-900 border-1 ">
+						<div className="grid grid-cols-2 gap-4">
+							<div className="flex flex-col gap-2">
+								<label htmlFor="nameFilter">Name filter</label>
+								<input
+									type="text"
+									id="nameFilter"
+									className="p-2 rounded bg-white text-black"
+									value={nameFilter}
+									onChange={(e) =>
+										setNameFilter(e.target.value)
+									}
+								/>
+							</div>
+
+							<div className="flex flex-col gap-2">
+								<label htmlFor="colorFilter">
+									Color filter
+								</label>
+								<input
+									type="text"
+									id="colorFilter"
+									className="p-2 rounded bg-white text-black"
+									value={colorFilter}
+									onChange={(e) =>
+										setColorFilter(e.target.value)
+									}
+								/>
+							</div>
+
+							<div className="flex flex-col gap-2">
+								<label>Price filter</label>
+								<div className="flex gap-4">
+									<div className="flex flex-col gap-2">
+										<label
+											htmlFor="fromPriceFilter"
+											className="text-sm"
+										>
+											From
+										</label>
+										<input
+											type="text"
+											id="fromPriceFilter"
+											className="p-2 rounded bg-white text-black w-20"
+											value={fromPriceFilter}
+											onChange={(e) =>
+												setFromPriceFilter(
+													e.target.value
+												)
+											}
+										/>
+									</div>
+									<div className="flex flex-col gap-2">
+										<label
+											htmlFor="toPriceFilter"
+											className="text-sm"
+										>
+											To
+										</label>
+										<input
+											type="text"
+											id="toPriceFilter"
+											className="p-2 rounded bg-white text-black w-20"
+											value={toPriceFilter}
+											onChange={(e) =>
+												setToPriceFilter(e.target.value)
+											}
+										/>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="text-center mt-4">
+							<button
+								className="bg-white text-blue-900 px-4 py-2 rounded"
+								onClick={applyFilers}
+							>
+								Apply Filters
+							</button>
+							<button
+								className="bg-white text-blue-900 px-4 py-2 rounded ml-4"
+								onClick={clearFilters}
+							>
+								Clear Filters
+							</button>
+						</div>
+					</div>
+
 					<ItemList type={"basketType"} items={items}></ItemList>
 					<div className="flex justify-end gap-3 mt-2 items-center">
 						<button
